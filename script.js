3df239c6abe8ac18455ed7159b8cbedb245e03cd
Rahul Das <dasrahulme123@gmail.com>
2024-03-05 09:43:15 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+  lerp: 0.1,
+});
+
+document.querySelector("#footer .btt").addEventListener("click", function () {
+  scroll.scrollTo(0);
+});
+
+function loader() {
+  let tl = gsap.timeline();
+  tl.to("#yellow", {
+    top: "-100%",
+    delay: 0.5,
+    duration: 0.5,
+    ease: "expo.out",
+  });
+
+  tl.to(
+    "#loader video",
+    {
+      top: "-100%",
+      delay: 0.5,
+      duration: 0.4,
+      ease: "expo.out",
+    },
+    "anim"
+  );
+  tl.to(
+    "#loader h1",
+    {
+      color: "black",
+      delay: 0.5,
+      duration: 0.9,
+    },
+    "anim"
+  );
+
+  tl.to(
+    "#loader",
+    {
+      opacity: 0,
+      delay: 0.4,
+      duration: 0.4,
+      ease: "expo.out",
+    },
+    "anim"
+  );
+  tl.to("#loader", {
+    display: "none",
+    delay: 0.01,
+  });
+}
+loader();
+
+function page2bganim() {
+  let elems = document.querySelectorAll(".elem");
+  let page2 = document.querySelector("#page2");
+  elems.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+      let bgimg = elem.getAttribute("data-img");
+      page2.style.backgroundImage = `url(${bgimg})`;
+      // page2.style.backgroundColor = "transparent";
+    });
+
+    elem.addEventListener("mouseleave", function () {
+      page2.style.backgroundImage = "none";
+      page2.style.backgroundColor = "black";
+    });
+  });
+}
+page2bganim();
