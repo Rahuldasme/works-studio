34f114be5fc823c7a484a9ac2b27a1ee63eacf67
Rahul Das <dasrahulme123@gmail.com>
2024-03-07 17:42:19 +0545

    added the auto change background feature on page 2

--- script.js
+++ script.js
@@ -1,77 +1,101 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-  lerp: 0.1,
-});
-
-document.querySelector("#footer #btt").addEventListener("click", function () {
-  scroll.scrollTo(0);
-});
-
-function loader() {
-  let tl = gsap.timeline();
-  tl.to("#yellow", {
-    top: "-100%",
-    delay: 0.5,
-    duration: 0.5,
-    ease: "expo.out",
+document.addEventListener("DOMContentLoaded", function () {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+    lerp: 0.1,
   });
 
-  tl.to(
-    "#loader video",
-    {
+  document.querySelector("#footer #btt").addEventListener("click", function () {
+    scroll.scrollTo(0);
+  });
+
+  function loader() {
+    let tl = gsap.timeline();
+    tl.to("#yellow", {
       top: "-100%",
-      delay: 0.4,
+      delay: 0.5,
       duration: 0.5,
       ease: "expo.out",
-    },
-    "anim"
-  );
-  tl.to(
-    "#loader h1",
-    {
-      color: "black",
-      delay: 0.5,
-      duration: 0.9,
-    },
-    "anim"
-  );
-
-  tl.to(
-    "#loader",
-    {
-      opacity: 0,
-      delay: 0.3,
-      duration: 2,
-      ease: "expo.out",
-    },
-    "anim"
-  );
-  tl.to(
-    "#loader",
-    {
-      display: "none",
-      delay: 0.01,
-    },
-    "anim"
-  );
-}
-loader();
-
-function page2bganim() {
-  let elems = document.querySelectorAll(".elem");
-  let page2 = document.querySelector("#page2");
-  elems.forEach(function (elem) {
-    elem.addEventListener("mouseenter", function () {
-      let bgimg = elem.getAttribute("data-img");
-      page2.style.backgroundImage = `url(${bgimg})`;
-      // page2.style.backgroundColor = "transparent";
     });
 
-    elem.addEventListener("mouseleave", function () {
-      page2.style.backgroundImage = "none";
-      page2.style.backgroundColor = "black";
+    tl.to(
+      "#loader video",
+      {
+        top: "-100%",
+        delay: 0.4,
+        duration: 0.5,
+        ease: "expo.out",
+      },
+      "anim"
+    );
+    tl.to(
+      "#loader h1",
+      {
+        color: "black",
+        delay: 0.5,
+        duration: 0.9,
+      },
+      "anim"
+    );
+
+    tl.to(
+      "#loader",
+      {
+        opacity: 0,
+        delay: 0.3,
+        duration: 2,
+        ease: "expo.out",
+      },
+      "anim"
+    );
+    tl.to(
+      "#loader",
+      {
+        display: "none",
+        delay: 0.01,
+      },
+      "anim"
+    );
+  }
+  loader();
+
+  function page2bganim() {
+    let elems = document.querySelectorAll(".elem");
+    let page2 = document.querySelector("#page2");
+    elems.forEach(function (elem) {
+      elem.addEventListener("mouseenter", function () {
+        let bgimg = elem.getAttribute("data-img");
+        page2.style.backgroundImage = `url(${bgimg})`;
+        // page2.style.backgroundColor = "transparent";
+      });
     });
-  });
-}
-page2bganim();
+
+    let bgImages = [
+      "img/MIN-1_240208_000417_7e2310f9f2d149843122e3a2929676cc.jpg",
+      "img/converse-21_7e2310f9f2d149843122e3a2929676cc.jpg",
+      "img/skky-25_7e2310f9f2d149843122e3a2929676cc.jpg",
+      "img/BOLDLY-1_7e2310f9f2d149843122e3a2929676cc.jpg",
+      "img/USS-1_240208_001956_7e2310f9f2d149843122e3a2929676cc.jpg",
+    ];
+    let bgIndex = 0;
+
+    function changeBackground() {
+      page2.style.backgroundImage = `url(${bgImages[bgIndex]})`;
+      bgIndex = (bgIndex + 1) % bgImages.length;
+    }
+
+    let bgTimer = setInterval(changeBackground, 2000);
+
+    page2.addEventListener("mouseenter", function () {
+      clearInterval(bgTimer);
+    });
+
+    page2.addEventListener("mouseleave", function () {
+      bgTimer = setInterval(changeBackground, 2000);
+    });
+
+    // Initially set the background image
+    changeBackground();
+  }
+  page2bganim();
+});
